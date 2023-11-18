faa1256385ffbbd72dbfcfecb9b8440a8ca28841
roy <baron.roy@gmail.com>
2023-11-18 16:36:44 -0500

    Adds player health

--- scenes/game_objects/player/player.gd
+++ scenes/game_objects/player/player.gd
@@ -3,6 +3,18 @@ extends CharacterBody2D
 const MAX_SPEED := 120
 const ACCELERATION_SMOOTHING = 25
 
+@onready var collision_area: Area2D = $CollisionArea2D
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var damage_interval_timer: Timer = $DamageIntervalTimer
+
+var number_colliding_bodies := 0
+
+
+func _ready():
+	collision_area.body_entered.connect(on_body_entered)
+	collision_area.body_exited.connect(on_body_exited)
+	damage_interval_timer.timeout.connect(on_damage_interval_timeout)
+
 
 func _process(delta):
 	var movement_vector = get_movement_vector()
@@ -21,3 +33,25 @@ func get_movement_vector():
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 
 	return Vector2(x_movement, y_movement)
+
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+
+	health_component.damage(1)
+	damage_interval_timer.start()
+	print(health_component.current_health)
+
+
+func on_body_entered(_other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+
+func on_body_exited(_other_body: Node2D):
+	number_colliding_bodies -= 1
+
+
+func on_damage_interval_timeout():
+	check_deal_damage()
