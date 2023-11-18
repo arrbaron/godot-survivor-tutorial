04e08170f86beaa2e3c9dded3d98f0f30dcf4948
roy <baron.roy@gmail.com>
2023-11-18 09:41:21 -0500

    Sets up global signals for exp tracking

---
+++ scenes/managers/experience_manager.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var current_experience := 0
+
+
+func _ready():
+	GameEvents.experience_vial_collected.connect(on_experience_vial_collected)
+
+
+func increment_experience(number: int):
+	current_experience += number
+	print(current_experience)
+
+
+func on_experience_vial_collected(number: int):
+	increment_experience(number)
